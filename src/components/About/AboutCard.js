de8c5ce1b7677b9e764a79f77d180c948d485395
Sharukh Dastageer Shaikh <shaikhsharukh1963@gmail.com>
2023-01-20 18:49:40 +0530

    FILE added

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Sharukh Shaikh </span>
+            from <span className="purple"> Bangalore, India.</span>
+            <br />I am a Full Stack Web Developer specialized in MERN Stack.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Listening to Podcast
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Sharukh</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+// import leaf from "../../Assets/Projects/leaf.png";
+// import emotion from "../../Assets/Projects/emotion.png";
+import editor from "../../Assets/Projects/codeEditor.png";
+import chatify from "../../Assets/Projects/chatify.png";
+// import suicide from "../../Assets/Projects/suicide.png";
+import bitsOfCode from "../../Assets/Projects/blog.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="JDs Store"
+              description="JDs Store is an online Ecommerece store for fashion brands like clothing, wearbles, Footwears "
+              ghLink="https://github.com/sharukh230/JDs-Shop"
+              demoLink="https://jds-store.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bitsOfCode}
+              isBlog={false}
+              title="ESPN CricInfo"
+              description="EspnCricInfo is a sports news website exclusively for the game of cricket. It contains various news, columns, blogs, videos and fantasy sports games. Among its most popular feature are its liveblogs of cricket matches, which includes a bevy of scorecard options, allowing readers to track such aspects of the game as wagon wheels and partnership breakdowns."
+              ghLink="https://github.com/sbj1198/espn_cricinfo"
+              demoLink="https://espncric-info.netlify.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={editor}
+              isBlog={false}
+              title="Rodan+Field Clone"
+              description="Rodan + Fields is a leading skincare brand founded in 2000 by Stanford-trained Dermatologists Dr. Katie Rodan and Dr. Kathy Fields with a passion for giving people the best skin of their lives — and the confidence that comes with it. Rodan + Fields skincare philosophy is based on Multi-Med Therapy: The right ingredients in the right formulations in the right order deliver visible results. With its product efficacy and disruptive business model, Rodan + Fields is life-changing skincare."
+              ghLink="https://github.com/sharukh230/prickly-expert-9128"
+              demoLink="https://jovial-biscochitos-b40ea6.netlify.app/"              
+            />
+          </Col>
+          </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;

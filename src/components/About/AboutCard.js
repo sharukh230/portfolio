6e4e02b125c332b93b086d26f7ec4e6e1fcab4f4
Sharukh Dastageer Shaikh <shaikhsharukh1963@gmail.com>
2023-01-21 21:29:26 +0530

    image added

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,10 +8,16 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Sharukh Shaikh </span>
+            {/* Hi Everyone, I am <span className="purple">Sharukh Shaikh </span>
             from <span className="purple"> Bangalore, India.</span>
             <br />I am a Full Stack Web Developer specialized in MERN Stack.
             <br />
+            <br />
+            Apart from coding, some other activities that I love to do! */}
+            Hello! I'm <span className="purple">Sharukh Shaikh </span> a Software developer. 
+            I am experienced in taking fullstack applications from scratch to production.
+            Over the last one year, I've had the opportunity to develop for a handful of awesome projects/assignments and continue to do so today. 
+            My work includes developing making web apps, creating backend servers, and implementing desktop apps.
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,10 +4,11 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 // import leaf from "../../Assets/Projects/leaf.png";
 // import emotion from "../../Assets/Projects/emotion.png";
-// import editor from "../../Assets/Projects/codeEditor.png";
-// import chatify from "../../Assets/Projects/chatify.png";
-// import suicide from "../../Assets/Projects/suicide.png";
+import editor from "../../Assets/rodan1.png";
+import chatify from "../../Assets/jdstore1.png";
 // import bitsOfCode from "../../Assets/Projects/blog.png";
+import bitsOfCode from "../../Assets/espn1.png";
+// import suicide from "../../Assets/Projects/suicide.png";
 
 function Projects() {
   return (
@@ -23,10 +24,10 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              // imgPath={chatify}
+              imgPath={chatify}
               isBlog={false}
               title="JDs Store"
-              description="JDs Store is an online Ecommerece store for fashion brands like clothing, wearbles, Footwears "
+              description="JD's Store is an online Ecommerece store for fashion brands like clothing, wearbles, Footwears "
               ghLink="https://github.com/sharukh230/JDs-Shop"
               demoLink="https://jds-store.vercel.app/"
             />
@@ -34,7 +35,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              // imgPath={bitsOfCode}
+              imgPath={bitsOfCode}
               isBlog={false}
               title="ESPN CricInfo"
               description="EspnCricInfo is a sports news website exclusively for the game of cricket. It contains various news, columns, blogs, videos and fantasy sports games. Among its most popular feature are its liveblogs of cricket matches, which includes a bevy of scorecard options, allowing readers to track such aspects of the game as wagon wheels and partnership breakdowns."
@@ -45,7 +46,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              // imgPath={editor}
+              imgPath={editor}
               isBlog={false}
               title="Rodan+Field Clone"
               description="Rodan + Fields is a leading skincare brand founded in 2000 by Stanford-trained Dermatologists Dr. Katie Rodan and Dr. Kathy Fields with a passion for giving people the best skin of their lives — and the confidence that comes with it. Rodan + Fields skincare philosophy is based on Multi-Med Therapy: The right ingredients in the right formulations in the right order deliver visible results. With its product efficacy and disruptive business model, Rodan + Fields is life-changing skincare."
